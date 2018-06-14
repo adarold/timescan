6129e27b8355fc6d6bd3b3ad834da67f370e5175
Alessandro Da Rold <darold@farmui02.ts.infn.it>
2018-06-14 13:44:59 +0200

    removed comment

--- frun.C
+++ frun.C
@@ -1,6 +1,4 @@
 void frun(string path){
-
-cout << "ecco: " << ("fitter t9 (\""+path+"\")").c_str() << endl;
     gROOT->ProcessLine(".L fitter.C+");
     gROOT->ProcessLine(("fitter t9 (\""+path+"\")").c_str());
     gROOT->ProcessLine("t9.Loop()");
