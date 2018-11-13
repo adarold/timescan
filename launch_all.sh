8cfdda87a9bfd7d4bc0459a357da52cb2c37155a
Alessandro Da Rold <alessandro.da.rold@cern.ch>
2018-11-13 17:56:42 +0100

    added code for the many_years.C script

--- launch_all.sh
+++ launch_all.sh
@@ -3,8 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-#for j in 2017 20181 20182; do
-for j in 20182; do
+for j in 2017 20181 20182; do
   YEAR=$j
 
   #Execute timeshape_analysis the first time
@@ -154,5 +153,15 @@ for j in 20182; do
   done
 
 done
+
+#Draws year comparison histograms
+for i in 0 1 2 3 4; do
+  RUN_CONDITION=$i
+  root-6.02 -l -q -b "many_years.C (\"${DIR}\",${RUN_CONDITION})"
+done
+
+
+
+
 
 rmdir LSFJOB_*
